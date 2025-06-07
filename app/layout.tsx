0c8c275ccbdd7180a59dd2c5b5e05c8f50625fc6
Will Beason <willbeason@gmail.com>
2025-06-07 12:13:28 -0500

    Move header logic to own folder
    Signed-off-by: Will Beason <willbeason@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,80 +1,8 @@
-// 'use client';
-
 import '@mantine/core/styles.css';
 
-import { IconChevronDown } from '@tabler/icons-react';
-import {
-  Burger,
-  Center,
-  Container,
-  Group,
-  mantineHtmlProps,
-  MantineProvider,
-  Menu,
-} from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import { MantineLogo } from '@mantinex/mantine-logo';
+import { mantineHtmlProps, MantineProvider } from '@mantine/core';
+import HeaderMenu from '@/components/HeaderMenu/HeaderMenu';
 import { theme } from '@/theme';
-import classes from './HeaderMenu.module.css';
-
-const links: { link: string; label: string; links: { link: string; label: string }[] }[] = [
-  { link: '/foo', label: 'Foo', links: [] },
-];
-
-export function HeaderMenu() {
-  const [opened, { toggle }] = useDisclosure(false);
-
-  const items = links.map((link) => {
-    const menuItems = link.links?.map((item) => (
-      <Menu.Item key={item.link}>{item.label}</Menu.Item>
-    ));
-
-    if (menuItems) {
-      return (
-        <Menu key={link.label} trigger="hover" transitionProps={{ exitDuration: 0 }} withinPortal>
-          <Menu.Target>
-            <a
-              href={link.link}
-              className={classes.link}
-              onClick={(event) => event.preventDefault()}
-            >
-              <Center>
-                <span className={classes.linkLabel}>{link.label}</span>
-                <IconChevronDown size={14} stroke={1.5} />
-              </Center>
-            </a>
-          </Menu.Target>
-          <Menu.Dropdown>{menuItems}</Menu.Dropdown>
-        </Menu>
-      );
-    }
-
-    return (
-      <a
-        key={link.label}
-        href={link.link}
-        className={classes.link}
-        onClick={(event) => event.preventDefault()}
-      >
-        {link.label}
-      </a>
-    );
-  });
-
-  return (
-    <header className={classes.header}>
-      <Container size="md">
-        <div className={classes.inner}>
-          <MantineLogo size={28} />
-          <Group gap={5} visibleFrom="sm">
-            {items}
-          </Group>
-          <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
-        </div>
-      </Container>
-    </header>
-  );
-}
 
 export default function RootLayout({ children }: { children: any }) {
   return (
