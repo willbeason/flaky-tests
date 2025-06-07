a99b4d828711dee4c35fa9979c395c470c04657f
Will Beason <willbeason@gmail.com>
2025-06-07 18:10:20 -0500

    Add TurnPhase element and tests
    Signed-off-by: Will Beason <willbeason@gmail.com>

---
+++ components/TurnPhase/__test__/TurnPhase.test.tsx
@@ -0,0 +1,46 @@
+import TurnPhase, { TurnPhases, TurnPhasesOrder } from '@/components/TurnPhase/TurnPhase';
+import { render, screen } from '@/test-utils';
+
+describe('TurnPhase', () => {
+  describe('lists all phases', () => {
+    Object.entries(TurnPhases).forEach(([_, phase]) => {
+      it(`when in phase ${phase}`, () => {
+        render(<TurnPhase phase={phase} />);
+
+        expect(screen.queryByText(TurnPhases.GUESS)).toBeInTheDocument();
+        expect(screen.queryByText(TurnPhases.TEST)).toBeInTheDocument();
+        expect(screen.queryByText(TurnPhases.DEDUCE)).toBeInTheDocument();
+      });
+    });
+  });
+
+  describe('highlights the current turn phase', () => {
+    Object.entries(TurnPhasesOrder).forEach(([_, phase]) => {
+      it(`when in phase ${phase}`, () => {
+        render(<TurnPhase phase={phase} />);
+
+        const playLabel = screen.getByText(phase);
+
+        expect(playLabel).toHaveClass('active');
+      });
+    });
+  });
+
+  describe('does not highlight inactive phases', () => {
+    Object.entries(TurnPhases).forEach(([_, current_phase]) => {
+      it(`when in phase ${current_phase}`, () => {
+        render(<TurnPhase phase={current_phase} />);
+
+        Object.entries(TurnPhasesOrder).forEach(([_, phase]) => {
+          if (phase === current_phase) {
+            return;
+          }
+
+          const playLabel = screen.getByText(phase);
+
+          expect(playLabel).toHaveClass('inactive');
+        });
+      });
+    });
+  });
+});
